70677c075552c5717076c1f89f6810587f5f9e8f
Luca Yan <lucayan0506>
2022-05-04 21:25:48 +0100

    add level 4 (completed) and level 5 (not yet)

--- Assets/Scripts/TeleportCharacter.cs
+++ Assets/Scripts/TeleportCharacter.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TeleportCharacter : MonoBehaviour
 {
     public GameObject Character;
+    public GameObject teleporting;
+    public Vector3 pos;
+
+    private bool inside;
+
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-        Character.transform.position = new Vector3(-6.487956f, 1.43803f, 14.43859f);
+        inside = true;
+        teleporting.SetActive(true);
+        StartCoroutine(delay(2));
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inside = false;
+        teleporting.SetActive(false);
+    }
+    IEnumerator delay(int second)
+    {
+        yield return new WaitForSeconds(second);
+        if (inside)
+        {
+            Character.transform.position = pos;
+            teleporting.SetActive(false);
+        }
+
     }
 }
